8612a3c10097b8c68c5555424bf60c0ea0bcf2ed
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-06-23 19:15:42 -0300

    fix: 1-two-sum.go comment

--- go/1-two-sum.go
+++ go/1-two-sum.go
@@ -1,15 +1,17 @@
+// https://leetcode.com/problems/two-sum/
+
 func twoSum(nums []int, target int) []int {
-    for i := range nums {
-        for j := range nums {
-            if i == j {
-                continue
-            }
+	for i := range nums {
+		for j := range nums {
+			if i == j {
+				continue
+			}
 
-            if nums[i] + nums[j] == target {
-                return []int{i, j}
-            }
-        }
-    }
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
 
-    return []int{}
+	return []int{}
 }
