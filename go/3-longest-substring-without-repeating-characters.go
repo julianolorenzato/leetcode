79ff197490abea47c01d32dbc81fb87b4c63d124
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-12-25 23:04:42 -0300

    new(go): 3-longest-susbtring-without-repeating-characters

---
+++ go/3-longest-substring-without-repeating-characters.go
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/longest-substring-without-repeating-characters
+package leetcode
+
+func lengthOfLongestSubstring(s string) int {
+    max := 0
+
+    have := make(map[rune]int)
+
+    start := 0
+
+    for end, v := range s {
+        for have[v] != 0 {
+            have[rune(s[start])]--
+            start++
+        }
+
+        have[v]++
+
+        if end - start + 1 > max {
+            max = end - start + 1
+        }
+    }
+
+    return max
+}
